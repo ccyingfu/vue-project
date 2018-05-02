6a522897ffb00ed473c54802b1bc36bb6fbbffa9
liyingfu <cnyingfu@163.com>
2018-05-02 18:11:19 +0800

    commit a changing file.

--- src/components/widget/templ.vue
+++ src/components/widget/templ.vue
@@ -1,14 +1,14 @@
 <template>
     <div>
         <el-dialog
-        title="提示"
+        title="Tip"
         :visible.sync="vis"
         width="30%"
         :before-close="handleClose">
-        <span>这是一段信息</span>
+        <span>This is a message about testing component.</span>
         <span slot="footer" class="dialog-footer">
-            <el-button @click="close">取 消</el-button>
-            <el-button type="primary" @click="close">确 定</el-button>
+            <el-button @click="close">Cancel</el-button>
+            <el-button type="primary" @click="close">True</el-button>
         </span>
         </el-dialog>
     </div>
